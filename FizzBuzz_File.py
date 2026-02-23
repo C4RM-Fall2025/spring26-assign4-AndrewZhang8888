1115f89bd4193fdd12d86f86deafe9138b618aac
AndrewZhang1570 <53244790+AndrewZhang8888@users.noreply.github.com>
2026-02-23 12:03:24 -0500

    Change FizzBuzz output strings to lowercase

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -3,11 +3,12 @@ def FizzBuzz(start, finish):
     # We use finish + 1 because range() is exclusive of the stop value
     for i in range(start, finish + 1):
         if i % 3 == 0 and i % 5 == 0:
-            result.append("FizzBuzz")
+            result.append("fizzbuzz")
         elif i % 3 == 0:
-            result.append("Fizz")
+            result.append("fizz")
         elif i % 5 == 0:
-            result.append("Buzz")
+            result.append("buzz")
         else:
             result.append(i)
     return result
+
