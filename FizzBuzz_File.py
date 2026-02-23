de952d08ead5d585b62da38d5a50bde0b7926361
AndrewZhang1570 <53244790+AndrewZhang8888@users.noreply.github.com>
2026-02-23 11:53:14 -0500

    Implement FizzBuzz function with proper logic

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -1,5 +1,13 @@
-
-
 def FizzBuzz(start, finish):
-    v = ['buzz', 41, 'fizz', 43, 424, 'fizzbuzz']
-    return(v)
+    result = []
+    # We use finish + 1 because range() is exclusive of the stop value
+    for i in range(start, finish + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            result.append("FizzBuzz")
+        elif i % 3 == 0:
+            result.append("Fizz")
+        elif i % 5 == 0:
+            result.append("Buzz")
+        else:
+            result.append(i)
+    return result
